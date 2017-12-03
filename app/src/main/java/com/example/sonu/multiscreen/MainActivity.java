d524e9b017ee5771ac6cde8fe630ea10f7b88a06
sonub396 <sonub396@gmail.com>
2017-12-03 12:30:22 +0530

    Simple spinner added

--- app/src/main/java/com/example/sonu/multiscreen/MainActivity.java
+++ app/src/main/java/com/example/sonu/multiscreen/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.sonu.multiscreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] places_array;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialise array
+        this.places_array = new String[]{"Neredmet","Sainikpuri","R.K Puram"};
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.places_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                switch (i){
+                    case 0:
+                        Toast.makeText(getApplicationContext(),"1",Toast.LENGTH_SHORT).show();
+
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Nothing",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
